ba8f27ccdf374721878c4b0f1c9aba5f818a4d51
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-10-12 11:31:27 +0200

    homepage returns json from most-popular-movies.json

--- src/OMDb-api.js
+++ src/OMDb-api.js
@@ -1,5 +1,21 @@
 'use strict';
+const fetch = require('node-fetch');
+const popularMovies = require('./most-popular-movies.json');
 
-const NAME = {};
+const url = 'http://www.omdbapi.com';
+const key = 'cda0cff1';
 
-module.exports = NAME;
+const omdb = {
+    homepage: async () => {
+        return popularMovies.top10;
+        //return await Promise.all(popularMovies.ids.map(async id => await omdb.get(id)));
+    },
+
+    get: async (id) => {
+        return await fetch(`${url}/?i=${id}&apikey=${key}`).then(res => res.json());
+    }
+};
+
+omdb.homepage();
+
+module.exports = omdb;
