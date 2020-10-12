1304d0fa527b7869778b6837f8721a6b9308f8e9
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-10-12 13:38:13 +0200

    added search route

--- route/index.js
+++ route/index.js
@@ -14,4 +14,19 @@ router.get('/', async (req, res) => {
     res.render('index', data);
 });
 
+router.get('/search', async (req, res) => {
+    data = {
+        title: 'BIMobject code test'
+    };
+
+    const keyword = req.query.search;
+    const results = await omdb.search(keyword);
+
+    data.res = results.Search;
+
+    console.log(data.res);
+
+    res.render('index', data);
+});
+
 module.exports = router;
